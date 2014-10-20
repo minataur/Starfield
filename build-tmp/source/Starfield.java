b1d429dc49332605dbf76df7268230be6348176f
minac.98sf@gmail.com <michoe@697-334-Bree.sf.k12.internal>
2014-10-20 12:14:03 -0700

    Starfield

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -1,6 +1,22 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 static final int count = 100;
 Particle[] suzy = new Particle[count];
-void setup() {
+public void setup() {
 	size(600,600);
 	background(0);
 	
@@ -11,7 +27,7 @@ void setup() {
 
 	
 }
-void draw() {
+public void draw() {
 	noStroke();
 	fill(0,10);
 	rect(0,0,600,600);
@@ -41,7 +57,7 @@ class NormalParticle implements Particle{
 	public void move() {
 		myX+= Math.cos(angle)*speed;
 		myY+= Math.sin(angle)*speed;
-		angle+=0.02;
+		angle+=0.02f;
 
 	}
 }
@@ -87,3 +103,12 @@ class OddballParticle implements Particle{
 }
 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
